318642da66f67c06fe4820c4765bf8035704ab0d
dz-one1 <vivavoir150@gmail.com>
2025-11-23 21:58:34 +0100

    تحديث إعدادات المتجر - 23/11/2025 21:58:33

--- config.js
+++ config.js
@@ -413,20 +413,26 @@ const STORE_CONFIG = {
       "weightTo": 114,
       "enabled": false
     },
-    
+    "XS": {
+      "heightFrom": 150,
+      "heightTo": 160,
+      "weightFrom": 40,
+      "weightTo": 55,
+      "enabled": true
+    },
     "S": {
       "heightFrom": 160,
       "heightTo": 175,
       "weightFrom": 50,
       "weightTo": 65,
-      "enabled": false
+      "enabled": true
     },
     "M": {
       "heightFrom": 170,
       "heightTo": 180,
       "weightFrom": 65,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "L": {
       "heightFrom": 180,
@@ -449,7 +455,13 @@ const STORE_CONFIG = {
       "weightTo": 120,
       "enabled": false
     },
-   
+    "A": {
+      "heightFrom": 160,
+      "heightTo": 200,
+      "weightFrom": 50,
+      "weightTo": 120,
+      "enabled": true
+    },
     "S1": {
       "heightFrom": 160,
       "heightTo": 180,
