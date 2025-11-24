6c7513c5c6d8b483f2ffa4d4eeda686209b17441
dz-one1 <vivavoir150@gmail.com>
2025-11-24 13:24:59 +0100

    تحديث إعدادات المتجر - 24/11/2025 13:24:58

--- config.js
+++ config.js
@@ -1,26 +1,5 @@
 const STORE_CONFIG = {
-  "PRODUCTS": {
-    "1": {
-      "name": "Bigger nike",
-      "price": 5800,
-      "image": "https://raw.githubusercontent.com/dz-one1/sanix-dz/main/images/product-1-1763926812417.jpg",
-      "description": "",
-      "availableSizes": [
-        "S1",
-        "S",
-        "S2",
-        "S3"
-      ],
-      "availableColors": [
-        "أبيض",
-        "أسود",
-        "أحمر",
-        "أزرق"
-      ],
-      "ageRanges": [],
-      "colorImages": {}
-    }
-  },
+  "PRODUCTS": {},
   "DELIVERY_PRICES": {
     "01 - أدرار": {
       "home": 1100,
@@ -256,16 +235,14 @@ const STORE_CONFIG = {
     }
   },
   "DISCOUNTS": {
-    "enableQuantityDiscount": true,
-    "minQuantityForDiscount": 1,
-    "discountPerItem": 800,
-    "discountScope": "selected",
-    "discountProducts": [
-      1
-    ],
+    "enableQuantityDiscount": false,
+    "minQuantityForDiscount": 2,
+    "discountPerItem": 300,
+    "discountScope": "all",
+    "discountProducts": [],
     "enablePromotionalDiscount": false,
     "promotionalDiscountPercent": 20,
-    "promotionalDiscountedPrice": null,
+    "promotionalDiscountedPrice": 0,
     "promoDiscountScope": "all",
     "promoDiscountProducts": []
   },
@@ -278,7 +255,7 @@ const STORE_CONFIG = {
       "0xxxxxxxxx",
       "0xxxxxxxxx"
     ],
-    "logo": "https://raw.githubusercontent.com/dz-one1/sanix-dz/main/images/product-store-logo-1763927044495.jpg",
+    "logo": "",
     "facebookUrl": "",
     "instagramUrl": "",
     "messengerUrl": "",
@@ -287,9 +264,9 @@ const STORE_CONFIG = {
     "whatsappUrl": ""
   },
   "GOOGLE_SHEETS": {
-    "url": "https://docs.google.com/spreadsheets/d/1ostnWuYQnG4KwkbKCRW1MHw5wJNBTAA8NsvIh7RIfx8/edit?gid=0#gid=0",
+    "url": "",
     "description": "",
-    "autoUpdate": true
+    "autoUpdate": false
   },
   "AVAILABLE_COLORS": [
     "أبيض",
@@ -334,170 +311,168 @@ const STORE_CONFIG = {
       "heightTo": 160,
       "weightFrom": 50,
       "weightTo": 70,
-      "enabled": false
+      "enabled": true
     },
     "39": {
       "heightFrom": 152,
       "heightTo": 162,
       "weightFrom": 53,
       "weightTo": 74,
-      "enabled": false
+      "enabled": true
     },
     "40": {
       "heightFrom": 154,
       "heightTo": 164,
       "weightFrom": 56,
       "weightTo": 78,
-      "enabled": false
+      "enabled": true
     },
     "41": {
       "heightFrom": 156,
       "heightTo": 166,
       "weightFrom": 59,
       "weightTo": 82,
-      "enabled": false
+      "enabled": true
     },
     "42": {
       "heightFrom": 158,
       "heightTo": 168,
       "weightFrom": 62,
       "weightTo": 86,
-      "enabled": false
+      "enabled": true
     },
     "43": {
       "heightFrom": 160,
       "heightTo": 170,
       "weightFrom": 65,
       "weightTo": 90,
-      "enabled": false
+      "enabled": true
     },
     "44": {
       "heightFrom": 162,
       "heightTo": 172,
       "weightFrom": 68,
       "weightTo": 94,
-      "enabled": false
+      "enabled": true
     },
     "45": {
       "heightFrom": 164,
       "heightTo": 174,
       "weightFrom": 71,
       "weightTo": 98,
-      "enabled": false
+      "enabled": true
     },
     "46": {
       "heightFrom": 166,
       "heightTo": 176,
       "weightFrom": 74,
       "weightTo": 102,
-      "enabled": false
+      "enabled": true
     },
     "47": {
       "heightFrom": 168,
       "heightTo": 178,
       "weightFrom": 77,
       "weightTo": 106,
-      "enabled": false
+      "enabled": true
     },
     "48": {
       "heightFrom": 170,
       "heightTo": 180,
       "weightFrom": 80,
       "weightTo": 110,
-      "enabled": false
+      "enabled": true
     },
     "49": {
       "heightFrom": 172,
       "heightTo": 182,
       "weightFrom": 83,
       "weightTo": 114,
-      "enabled": false
+      "enabled": true
     },
-   
     "S": {
       "heightFrom": 160,
       "heightTo": 175,
       "weightFrom": 50,
       "weightTo": 65,
-      "enabled": false
+      "enabled": true
     },
     "M": {
       "heightFrom": 170,
       "heightTo": 180,
       "weightFrom": 65,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "L": {
       "heightFrom": 180,
       "heightTo": 195,
       "weightFrom": 70,
       "weightTo": 90,
-      "enabled": false
+      "enabled": true
     },
     "XL": {
       "heightFrom": 185,
       "heightTo": 200,
       "weightFrom": 85,
       "weightTo": 110,
-      "enabled": false
+      "enabled": true
     },
     "XXL": {
       "heightFrom": 190,
       "heightTo": 205,
       "weightFrom": 95,
       "weightTo": 120,
-      "enabled": false
+      "enabled": true
     },
-    
     "S1": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "S2": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "S3": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "M1": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "M2": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "M3": {
       "heightFrom": 160,
       "heightTo": 180,
       "weightFrom": 50,
       "weightTo": 80,
-      "enabled": false
+      "enabled": true
     },
     "XXXL": {
       "heightFrom": 195,
       "heightTo": 210,
       "weightFrom": 105,
       "weightTo": 140,
-      "enabled": false
+      "enabled": true
     }
   }
 };
