2474e1ead6e0fdaf2b85daef869bf92b2ffa97e5
dz-one1 <vivavoir150@gmail.com>
2025-11-27 16:45:55 +0100

    تحديث إعدادات المتجر - 27/11/2025 16:45:53

--- config.js
+++ config.js
@@ -266,7 +266,7 @@ const STORE_CONFIG = {
       "0xxxxxxxxx",
       "0xxxxxxxxx"
     ],
-    "logo": "https://raw.githubusercontent.com/dz-one1/sanix-dz/main/images/store-logo-1764245727709-product-store-logo-1763847608230.jpg",
+    "logo": "https://raw.githubusercontent.com/dz-one1/sanix-dz/main/images/store-logo-1764258344474-product-store-logo-1763847608230.jpg",
     "facebookUrl": "",
     "instagramUrl": "",
     "messengerUrl": "",
